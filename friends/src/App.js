891c2878b6fae6e1563654f04f6dcd5456cd019b
unknown <eastkim1@hotmail.com>
2020-02-26 10:53:05 -0700

    working on login

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
+import Login from './components/login';
+import PrivateRoute from './components/PrivateRoute';
 
 function App() {
   return (
